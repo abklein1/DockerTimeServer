38325d9f232953560717cd623ac52489fd918f99
Alex Klein <89855699+abklein1@users.noreply.github.com>
2023-02-10 10:42:34 -0600

    Update with container

--- TimeServer/src/main/java/com/example/timeserver/TimeController.java
+++ TimeServer/src/main/java/com/example/timeserver/TimeController.java
@@ -1,8 +1,11 @@
 package com.example.timeserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimeController {
 	
 	@RequestMapping(value = "/time", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getTime() {
+	public ResponseEntity<Map<String, String>> getTime() {
+		
 		Timer timer = new Timer();
-		return timer.getTime();
+		
+		try {
+			Map<String, String> body = new HashMap<>();
+			body.put("datetime", timer.getTime());
+			return new ResponseEntity<>(body, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 }
